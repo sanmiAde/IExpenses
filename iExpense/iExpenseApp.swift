118ddc6868ebd0ca00f711b6ffbe11f9183d3c0d
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-19 07:11:31 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by sanmi_personal on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
